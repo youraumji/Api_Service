0c604c23272361ae216c2e806b33b29f295295d3
youraumji <jiratchaya.aum.m@gmail.com>
2025-02-09 22:17:16 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mytestapp/my_stateful_widget.dart';
-import 'package:mytestapp/page_answer/answer1.dart';
-import 'package:mytestapp/page_answer/answer2.dart';
-import 'package:mytestapp/page_answer/answer3.dart';
-import 'package:mytestapp/page_answer/answer4.dart';
-import 'package:mytestapp/page_answer/answer_portal.dart';
+import 'package:mytestapp/AirQuality.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,34 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title: 'HomeWork2',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const answer_portal(),
-    );
-  }
-}
-
-class GreetingWidget extends StatelessWidget {
-  final String name;
-  const GreetingWidget({super.key, required this.name});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Demo APP'),
-        backgroundColor: Colors.orange,
-      ),
-      body: Center(
-        child: Text('Hello, $name'),
-      ),
+      home: AirQuality(),
     );
   }
 }

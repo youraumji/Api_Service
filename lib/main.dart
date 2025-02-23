8fef9b8b1cd823342de0f0c8dc52ddcf3e91f034
youraumji <jiratchaya.aum.m@gmail.com>
2025-02-23 21:45:31 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mytestapp/api_service.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
